f7cb23e3d1390a430ab8037717d8f1e70de21a3e
AnnBond <annbondar4uk@gmail.com>
2017-08-25 00:42:54 +0300

    form for login view

---
+++ resources/views/loginForm.php
@@ -0,0 +1,13 @@
+<div class="loginForm">
+    <form method="post" action="<?= \app\core\createUrl('login') ?>">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Login</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="userName">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="userPassword">
+        </div>
+        <button type="submit" class="btn btn-primary" name="login">Login</button>
+    </form>
+</div>
