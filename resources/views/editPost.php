2b8e0dc5dcb31031708fef3aa95c46e697896621
AnnBond <annbondar4uk@gmail.com>
2017-08-27 14:46:10 +0300

    bootstrap template add

--- resources/views/editPost.php
+++ resources/views/editPost.php
@@ -1,14 +1,17 @@
 <section class="editPost clearfix">
     <form class="newPost" method="post" action="<?= \app\core\createUrl('edit_post', ['id' => $post['id']]) ?>">
-        <label>Title: <br>
-            <input type="text" class="add-post-title" name="title" placeholder="Title of post" value="<?= $post['title'] ?>" required/>
-        </label>
-        <label>Cost:
-            <input type="text" class="add-post-title" name="cost" placeholder="Title of post" value="<?= $post['cost'] ?>" required/>
-        </label>
-        <label>Post:
-            <textarea name="description" cols="74" rows="10" placeholder="Some interesting..."><?= $post['description'] ?></textarea>
-        </label>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Title:</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="title" placeholder="Title" value="<?= $post['title'] ?>" required />
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Cost: </label>
+            <input type="number" class="form-control" placeholder="Cost" name="cost" value="<?= $post['cost'] ?>" required />
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Description</label>
+            <textarea class="form-control" rows="3" placeholder="Some interesting..." name="description"><?= $post['description'] ?></textarea>
+        </div>
         <button type="submit" class="submit" name="edit">Submit</button>
     </form>
 </section>
