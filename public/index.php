590d99cccba7b8494d42912f034b3e1c8057e460
AnnBond <annbondar4uk@gmail.com>
2017-08-27 14:41:22 +0300

    add try catch exceptions

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 include '../app/app.php';
 
 use app\core;
+use app\exceptions\HttpNotFoundException;
 
 global $app;
 
-$app['route'] = core\getCurrentRoute();
-
-if(!empty($app['route'] )) {
+try {
+    $app['route'] = core\getCurrentRoute();
     echo core\renderFile($app['route']['file'], $app['route']['function'], $app['route']['params']);
+} catch (HttpNotFoundException $e) {
+    echo core\renderFile('error.php', 'app\\src\\error\\httpNotFoundError', [$e->getMessage()]);
+    exit();
+} catch (\Exception $e) {
+    echo core\renderFile('error.php', 'app\\src\\error\\internalServerError', [$e->getMessage()]);
+    exit();
 }
 
