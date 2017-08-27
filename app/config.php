188bfd69135b72725a3e9144ad65607e6d9b3784
AnnBond <annbondar4uk@gmail.com>
2017-08-27 14:11:22 +0300

    Connection to db use capsule->addConnection

--- app/config.php
+++ app/config.php
@@ -1,8 +1,19 @@
 <?php
-return [
+/*return [
     'name' => 'Mini Shop',
     'db_host' => 'localhost',
     'db_name' => 'shop_db',
     'db_username' => 'root',
     'db_password' => ''
-];
+];*/
+
+ $capsule->addConnection([
+     'driver'    => 'mysql',
+     'host'      => 'localhost',
+     'database'  => 'shop_db',
+     'username'  => 'root',
+     'password'  => '',
+     'charset'   => 'utf8',
+     'collation' => 'utf8_unicode_ci',
+     'prefix'    => '',
+ ]);
