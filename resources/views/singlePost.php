27e0f2c543808e36f58772f9f85bfb625d952ef3
AnnBond <annbondar4uk@gmail.com>
2017-08-27 23:00:51 +0300

    add author photo on singlePost page

--- resources/views/singlePost.php
+++ resources/views/singlePost.php
@@ -1,17 +1,29 @@
 <div class="container">
     <div class="post singlePost">
-        <div class="card">
-            <!--<img class="card-img-top" src="..." alt="Card image cap">-->
-            <div class="card-body">
-                <p class="date"><?= $post['created_at'] ?></p>
-                <h4 class="card-title"><?= $post['title'] ?></h4>
-                <p class="cost"><?= $post['cost'] . "$" ?></p>
-                <p class="cost">Category: <?= $post['category_name'] ?></p>
-                <p class="cost">User: <?= $post['user_name'] ?></p>
-                <p class="card-text">Description: <?php print_r($post['description']); ?></p>
-                <a href="?=buy" class="btn btn-primary">Buy</a>
-            </div>
-        </div>
+        <?php foreach ($users as $user) :?>
+            <?php if( $user['id'] == $post['user_id']) :?>
+                <div class="card">
+                    <!--<img class="card-img-top" src="..." alt="Card image cap">-->
+                    <div class="row" style="padding: 20px;">
+                        <div class="card-body col-11">
+                            <p class="date"><?= $post['created_at'] ?></p>
+                            <h4 class="card-title"><?= $post['title'] ?></h4>
+                            <p class="cost"><?= $post['cost'] . "$" ?></p>
+                            <p class="cost">Category: <?= $post['category_name'] ?></p>
+
+                            <p class="card-text">Description: <?php print_r($post['description']); ?></p>
+                            <a href="?=buy" class="btn btn-primary">Buy</a>
+                        </div>
+                        <div class="author col-1">
+                            <p class="cost">Author: <br><?= $post['user_name'] ?></p>
+                            <div class="userImg">
+                                <a href="#" class="personalPhoto" style=" width: 60px; height: 100%; display: inline-block;"><img src="<?= $user['imgUser']; ?>" alt="" style="width: 100%; height: 100%;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
     </div>
 </div>
 
