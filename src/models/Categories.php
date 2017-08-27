aba662837d143b1a3a936576bf10a3e52cc378ea
AnnBond <annbondar4uk@gmail.com>
2017-08-27 14:57:21 +0300

    class Categories

---
+++ src/models/Categories.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ANN
+ * Date: 8/26/2017
+ * Time: 4:06 PM
+ */
+
+
+namespace app\src\models;
+
+
+class Categories extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = "categories";
+    public $timestamps = false;
+}
