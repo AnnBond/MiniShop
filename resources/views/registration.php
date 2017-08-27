78a145f8e8b0ea7937c1783f9f42a86c9fecf027
AnnBond <annbondar4uk@gmail.com>
2017-08-27 23:20:02 +0300

    update registration form(choose user photo)

--- resources/views/registration.php
+++ resources/views/registration.php
@@ -1,5 +1,5 @@
 <div class="registrationForm">
-    <form method="post" action="<?= \app\core\createUrl('registration') ?>">
+    <form method="post" action="<?= \app\core\createUrl('registration') ?>" enctype="multipart/form-data">
         <div class="form-group">
             <label for="exampleInputEmail1">Login</label>
             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="userLogin">
@@ -11,6 +11,10 @@
         <div class="form-group">
             <label for="exampleInputPassword1">Email</label>
             <input type="text" class="form-control" id="exampleInputPassword1" placeholder="email" name="userEmail">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">Choose your photo</label>
+            <input type="file" class="form-control-file" name="newUserPhoto">
         </div>
         <button type="submit" class="btn btn-primary" name="registration">Submit</button>
     </form>
