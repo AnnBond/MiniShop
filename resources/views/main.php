c8bc34787065a5b54eeecf41384e28763c1df0d4
AnnBond <annbondar4uk@gmail.com>
2017-08-30 16:46:38 +0300

    add clickable category(filter)

--- resources/views/main.php
+++ resources/views/main.php
@@ -7,7 +7,7 @@
                     <p class="date"><?= $post['created_at'] ?></p>
                     <h4 class="card-title"><?= $post['title'] ?></h4>
                     <p class="cost"><?= $post['cost'] . "$" ?></p>
-                    <p class="cost">Category: <?= $post['category_name'] ?></p>
+                    <p class="cost">Category: <a href="<?= \app\core\createUrl('categoryById', ['id' => $post['category_id']]) ?>" class="card-title"><?php print_r($post['category_name']); ?></a></p>
                     <p class="card-text"><?php print_r($post['description']); ?></p>
                     <a href="<?= \app\core\createUrl('single_post', ['id' => $post['id']]) ?>" class="btn btn-primary">See more</a>
                 </div>
