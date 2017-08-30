f258d7c204f33ea907902f677b4b65df356ffaa9
AnnBond <annbondar4uk@gmail.com>
2017-08-30 23:35:38 +0300

    add faker for posts

--- fixtures/faker_posts.php
+++ fixtures/faker_posts.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ANN
- * Date: 8/30/2017
- * Time: 10:30 PM
- */
+include '../app/app.php';
+
+use app\src\models\Post;
+
+$faker = Faker\Factory::create();
+
+for ($i = 1; $i <= 30; $i++) {
+    $post = new Post();
+    $post->title = $faker->text(40);
+    $post->slug = $faker->text(40);
+    $post->description = $faker->text(200);
+    $post->cost = $faker->biasedNumberBetween(100, 2000);
+    $post->user_id = $faker->biasedNumberBetween(1, 5);
+    $post->category_id = $faker->biasedNumberBetween(0, 10);
+    $post->created_at = $faker->dateTime();
+    $post->updated_at = $faker->dateTime();
+    $post->save();
+}
