697f81cd3c91099edc28cc306b46a895de868fb7
AnnBond <annbondar4uk@gmail.com>
2017-09-03 23:46:29 +0300

    add class for search form

--- resources/views/layouts/default_layout.php
+++ resources/views/layouts/default_layout.php
@@ -26,7 +26,7 @@
                         <a class="nav-link active" href="<?= \app\core\createUrl('categories') ?>">Categories</a>
                     </li>
                 </ul>
-                <form class="form-inline col-4">
+                <form class="form-inline col-4 searchForm">
                     <input class="form-control mr-sm-2 " type="text" placeholder="Search" aria-label="Search" name="search">
                     <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
                 </form>
