303e13ed2bc2167788272a54645de44cd85af7f6
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-04 19:09:15 +0300

    small fix

--- fixtures/faker_posts.php
+++ fixtures/faker_posts.php
@@ -12,7 +12,7 @@ for ($i = 1; $i <= 30; $i++) {
     $post->description = $faker->text(200);
     $post->cost = $faker->biasedNumberBetween(100, 2000);
     $post->user_id = $faker->biasedNumberBetween(1, 5);
-    $post->category_id = $faker->biasedNumberBetween(0, 10);
+    $post->category_id = $faker->biasedNumberBetween(1, 10);
     $post->created_at = $faker->dateTime();
     $post->updated_at = $faker->dateTime();
     $post->save();
