28b90db73bcadba73dd319a298f65e5a7b33daea
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-04 19:12:47 +0300

    small fix

--- resources/views/singlePost.php
+++ resources/views/singlePost.php
@@ -5,7 +5,7 @@
                 <div class="card">
                     <!--<img class="card-img-top" src="..." alt="Card image cap">-->
                     <div class="row" style="padding: 20px;">
-                        <div class="card-body col-11">
+                        <div class="card-body col-10">
                             <p class="date"><?= $post['created_at'] ?></p>
                             <h4 class="card-title"><?= $post['title'] ?></h4>
                             <p class="cost"><?= $post['cost'] . "$" ?></p>
@@ -14,7 +14,7 @@
                             <p class="card-text">Description: <?php print_r($post['description']); ?></p>
                             <a href="?=buy" class="btn btn-primary">Buy</a>
                         </div>
-                        <div class="author col-1">
+                        <div class="author col-2">
                             <p class="cost">Author: <br><?= $post['user_name'] ?></p>
                             <div class="userImg">
                                 <a href="#" class="personalPhoto" style=" width: 60px; height: 100%; display: inline-block;"><img src="<?= $user['imgUser']; ?>" alt="" style="width: 100%; height: 100%;"></a>
