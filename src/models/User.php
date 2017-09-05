15cfd11943581a55cccddc5b42621882206e04e1
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-05 17:06:49 +0300

    small fix

--- src/models/User.php
+++ src/models/User.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ANN
- * Date: 8/26/2017
- * Time: 4:05 PM
- */
 
 namespace app\src\models;
-
 
 class User extends \Illuminate\Database\Eloquent\Model
 {
