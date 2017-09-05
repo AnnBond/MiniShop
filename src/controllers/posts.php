36675f5a459e7d30f8d26c5720e9366e5973e490
AnnBond <annbondar4uk@gmail.com>
2017-09-05 23:52:59 +0300

    fix query

--- src/controllers/posts.php
+++ src/controllers/posts.php
@@ -13,7 +13,7 @@ use app\src\models\User;
  * */
 function singlePost($id) {
 
-    $post = Post::where('posts.id', '=', $id)
+    $post = Post::with('author', 'category')->where('posts.id', '=', $id)
         ->leftjoin('categories', function ($join)
     {
         $join->on('categories.id', '=', 'posts.category_id');
