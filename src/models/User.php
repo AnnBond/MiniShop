ffe4d7765f34bd4777055f428329585d81838ac0
AnnBond <annbondar4uk@gmail.com>
2017-09-05 23:14:39 +0300

    method for current user's posts

--- src/models/User.php
+++ src/models/User.php
@@ -6,7 +6,9 @@ class User extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = "users";
     public $timestamps = false;
+
     public function postsByUser() {
         return $this->hasMany(Post::class, 'user_id', 'id');
     }
 }
+
