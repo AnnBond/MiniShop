548ec324c8c71dd19a6405d5482771533ea6343e
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-05 17:04:38 +0300

    add relationship post-user

--- src/models/Post.php
+++ src/models/Post.php
@@ -2,13 +2,13 @@
 
 namespace app\src\models;
 
-/**
- * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
- * @method static Builder create(array $attributes = [])
- * @method public Builder update(array $values)
- */
 class Post extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = "posts";
     public $timestamps = false;
+
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
 }
