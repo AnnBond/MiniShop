6fc350d2430e7c8d74a5cfdbdc3ef650f81f9fed
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-05 18:04:58 +0300

    need investigation

--- resources/views/adminPanel.php
+++ resources/views/adminPanel.php
@@ -23,9 +23,11 @@
             <h6> Your posts: </h6>
             <br>
             <div class="row">
+               <!-- --><?php /*print_r($posts)*/?>
             <?php foreach ($posts as $post) :?>
             <div class="card col-4">
                 <div class="card-body">
+                    <p><?php print_r($post["posts_by_user"][0]['title'])?></p>
                     <h6><a href="<?= \app\core\createUrl('single_post', ['id' => $post['id']]) ?>"><?= $post['title'] ?></a></h6>
                     <br>
                     <a href="<?= \app\core\createUrl('edit_post', ['id' => $post['id']]) ?>" class="btn btn-success edit">Edit</a>
