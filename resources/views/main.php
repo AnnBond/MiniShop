5d68cbba49f63844f052d9d71316938ce72cd867
AnnBond <annbondar4uk@gmail.com>
2017-09-05 23:19:55 +0300

    fix

--- resources/views/main.php
+++ resources/views/main.php
@@ -2,7 +2,6 @@
     <div class="row">
         <?php foreach ($posts as $post): ?>
             <div class="card col-6" >
-                <!--<img class="card-img-top" src="..." alt="Card image cap">-->
                 <div class="card-body">
                     <p class="date"><?= $post['created_at'] ?></p>
                     <h4 class="card-title"><?= $post['title'] ?></h4>
