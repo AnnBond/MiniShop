81eea3f34a3cb67e1d26e905746abc658ca60154
AnnBond <annbondar4uk@gmail.com>
2017-09-05 23:35:31 +0300

    create method retrieve category for post

--- src/models/Post.php
+++ src/models/Post.php
@@ -11,5 +11,9 @@ class Post extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
+    public function category()
+    {
+        return $this->hasOne(Categories::class, 'id', 'category_id');
+    }
 }
 
