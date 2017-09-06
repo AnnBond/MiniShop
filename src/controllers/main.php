201af77628498a01a7cf72dce8bd73a8805f7865
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-06 16:46:08 +0300

    relationships and renderview

--- src/controllers/main.php
+++ src/controllers/main.php
@@ -3,22 +3,14 @@ namespace app\src\controllers\main;
 
 use function app\core\renderView;
 use function app\core\addFlash;
-use function app\core\redirect;
-use function app\core\getCurrentRoute;
 
-use app\core;
-use app\src\models\Categories;
 use app\src\models\Post;
 
 function index() {
     if (isset($_GET['search'])) {
-        $posts = Post::where('posts.title', 'LIKE', '%' . $_GET['search'] . '%')
+        $posts = Post::with('author', 'category')
+            ->where('posts.title', 'LIKE', '%' . $_GET['search'] . '%')
             ->orWhere('posts.description', 'LIKE', '%' . $_GET['search'] . '%')
-            ->leftjoin('categories', function ($join)
-        {
-            $join->on('categories.id', '=', 'posts.category_id');
-        } )
-            ->select('posts.*', 'categories.name as category_name')
             ->orderBy('created_at', 'desc')
             ->get()
             ->toArray();
@@ -26,12 +18,13 @@ function index() {
         addFlash('success', sprintf('Your search results for: ' . $_GET['search']));
 
     } else {
-        $posts = Post::with('author', 'category')->orderBy('created_at', 'desc')
+        $posts = Post::with('author', 'category')
+            ->orderBy('created_at', 'desc')
             ->get()
             ->toArray();
     }
 
-    return renderView(['layouts/default_layout_with_sidebar.php', 'main.php'], ['posts' => $posts]);
+    return renderView(['layouts/default_layout_with_sidebar.php', 'main/main.php'], compact('posts'));
 }
 
 
