69fc7a13c6960aea1b1f92293d531cc0aa190a11
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-06 17:54:53 +0300

    make foreign key

--- migration/first_migration_up.php
+++ migration/first_migration_up.php
@@ -2,6 +2,21 @@
 include '../app/app.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+Capsule::schema()->create('users', function (Illuminate\Database\Schema\Blueprint $table) {
+    // Auto-increment id
+    $table->increments('id');
+    $table->string('name')->unique();
+    $table->string('password');
+    $table->string('email')->unique();
+    $table->string('imgUser')->nullable();
+});
+Capsule::schema()->create('categories', function (Illuminate\Database\Schema\Blueprint $table) {
+    // Auto-increment id
+    $table->increments('id');
+    $table->string('name');
+    $table->string('description');
+});
+
 
 Capsule::schema()->create('posts', function (Illuminate\Database\Schema\Blueprint $table) {
     // Auto-increment id
@@ -12,23 +27,12 @@ Capsule::schema()->create('posts', function (Illuminate\Database\Schema\Blueprin
     $table->integer('cost');
     $table->timestamp('created_at')->useCurrent();
     $table->timestamp('updated_at')->useCurrent();
-    $table->integer('user_id');
-    $table->integer('category_id');
+    $table->integer('user_id')->unsigned();
+    $table->foreign('user_id')->references('id')->on('users');
+    $table->integer('category_id')->unsigned();
+    $table->foreign('category_id')->references('id')->on('categories');
 });
 
-Capsule::schema()->create('categories', function (Illuminate\Database\Schema\Blueprint $table) {
-    // Auto-increment id
-    $table->increments('id');
-    $table->string('name');
-    $table->string('description');
-});
 
-Capsule::schema()->create('users', function (Illuminate\Database\Schema\Blueprint $table) {
-    // Auto-increment id
-    $table->increments('id');
-    $table->string('name')->unique();
-    $table->string('password');
-    $table->string('email')->unique();
-    $table->string('imgUser')->nullable();
-});
+
 
