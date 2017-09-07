9d96a5578d39a094e98d040dc03e87a8ca6ec33d
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-07 16:47:17 +0300

    add function uploadProcess

--- core/security.php
+++ core/security.php
@@ -67,3 +67,18 @@ function getUserIp()
 function getUserAgent() {
     return (string)$_SERVER['HTTP_USER_AGENT'] ?? 'unknown';
 }
+
+function processUpload() {
+    global $app;
+
+    $file = $_FILES['userPhoto']['name'];
+    $file_tmp = $_FILES['userPhoto']['tmp_name'];
+    $file_ext = strtolower(end(explode('.', $file)));
+    $expansions = array("jpeg", "jpg", "png", "svg");
+
+    if (in_array($file_ext, $expansions)) {
+        move_uploaded_file($file_tmp, $app['kernel.uploads_dir'] . DIRECTORY_SEPARATOR . $file);
+
+        return $app['kernel.uploadsUsers_dir'] . DIRECTORY_SEPARATOR . $file;
+    }
+}
