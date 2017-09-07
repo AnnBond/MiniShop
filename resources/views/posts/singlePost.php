9f129311b80cb97a8f95453742ddba76cd2e96da
Anna_Bondarchuk <Anna_Bondarchuk@epam.com>
2017-09-07 11:52:11 +0300

    fix output single post

--- resources/views/posts/singlePost.php
+++ resources/views/posts/singlePost.php
@@ -1,28 +1,24 @@
 <div class="container">
     <div class="post singlePost">
-        <?php foreach ($users as $user) :?>
-            <?php if( $user['id'] == $post['user_id']) :?>
-                <div class="card">
-                    <div class="row" style="padding: 20px;">
-                        <div class="card-body col-10">
-                            <p class="date"><?= $post['created_at'] ?></p>
-                            <h4 class="card-title"><?= $post['title'] ?></h4>
-                            <p class="cost"><?= $post['cost'] . "$" ?></p>
-                            <p class="cost">Category:
-                                <a href="<?= \app\core\createUrl('categoryById', ['id' => $post['category_id']]) ?>" class="card-title"><?php print_r($post['category']['name']); ?></a>
-                            </p>
-                            <p class="card-text">Description: <?php print_r($post['description']); ?></p>
-                        </div>
-                        <div class="author col-2">
-                            <p class="cost">Author: <?php print_r($post['author']['name'] )?></p>
-                            <div class="userImg">
-                                <a href="#" class="personalPhoto" style=" width: 60px; height: 100%; display: inline-block;"><img src="<?= $user['imgUser']; ?>" alt="" style="width: 100%; height: 100%;"></a>
-                            </div>
-                        </div>
+        <div class="card">
+            <div class="row" style="padding: 20px;">
+                <div class="card-body col-10">
+                    <p class="date"><?= $post['created_at'] ?></p>
+                    <h4 class="card-title"><?= $post['title'] ?></h4>
+                    <p class="cost"><?= $post['cost'] . "$" ?></p>
+                    <p class="cost">Category:
+                        <a href="<?= \app\core\createUrl('categoryById', ['id' => $post['category_id']]) ?>" class="card-title"><?php print_r($post['category']['name']); ?></a>
+                    </p>
+                    <p class="card-text">Description: <?php print_r($post['description']); ?></p>
+                </div>
+                <div class="author col-2">
+                    <p class="cost">Author: <br> <?php print_r($post['author']['name'] )?></p>
+                    <div class="userImg">
+                        <a href="#" class="personalPhoto" style=" width: 60px; height: 100%; display: inline-block;"><img src="<?= $post['author']['imgUser']; ?>" alt="" style="width: 100%; height: 100%;"></a>
                     </div>
                 </div>
-            <?php endif; ?>
-        <?php endforeach; ?>
+            </div>
+        </div>
     </div>
 </div>
 
